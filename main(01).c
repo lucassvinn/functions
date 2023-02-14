d760bc3deacd1257fc2c136f730ce199a98ffc7d
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-02-14 17:43:08 -0300

    Create main(01).c

---
+++ main(01).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+char option(char opa) {
+    switch (opa) {
+        case 'a':
+            printf("Você selecionou A!");
+        break;
+        case 'b':
+            printf("Você escolheu B!");
+        break;
+    }
+}
+
+int main()
+{
+    char xarx, z;
+    
+    printf("Informe X: ");
+    scanf("%c", &xarx);
+    
+    z = option(xarx);
+
+    return 0;
+}
